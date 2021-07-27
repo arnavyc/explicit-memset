7bd50a1969a4fc7ce5cfe1d6ea3c99e446ba4dfc
arnavyc <arnavyc@outlook.com>
2021-07-27 09:12:54 +0530

    create explicit-memset.h

---
+++ explicit-memset.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021 arnavyc <arnavyc@outlook.com>
+ * SPDX-License-Identifier: 0BSD
+ */
+
+#ifndef AY_EXPLICIT_MEMSET_H
+#define AY_EXPLICIT_MEMSET_H
+
+#include <stddef.h>
+
+void *ay_explicit_memset(void *str, int c, size_t n);
+
+#endif /* AY_EXPLICIT_MEMSET_H */
